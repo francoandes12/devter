089212b86894aa728fe17517999d2437f99b6c7c
francoandes12 <francoande12@gmail.com>
2020-12-24 19:01:45 -0300

    fix 2 home

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,16 +1,18 @@
-import { useState, useEffect } from "react";
-import Devit from "@c/Devit";
+import { useEffect, useState } from "react";
+import Devit from "components/Devit";
 import useUser from "hooks/useUser";
 import { listenLatestDevits } from "firebase/client";
 import Link from "next/link";
-import Create from "../../components/Icons/Create";
-import Home from "../../components/Icons/Home";
-import Search from "../../components/Icons/Search";
+import Create from "components/Icons/Create";
+import Home from "components/Icons/Home";
+import Search from "components/Icons/Search";
 import { colors } from "styles/theme";
 import Head from "next/head";
+
 export default function HomePage() {
   const [timeline, setTimeline] = useState([]);
   const user = useUser();
+
   useEffect(() => {
     let unsubscribe;
     if (user) {
@@ -18,6 +20,7 @@ export default function HomePage() {
     }
     return () => unsubscribe && unsubscribe();
   }, [user]);
+
   return (
     <>
       <Head>
@@ -28,15 +31,15 @@ export default function HomePage() {
       </header>
       <section>
         {timeline.map(
-          ({ createdAt, img, id, userId, content, userName, avatar }) => (
+          ({ createdAt, img, id, userName, avatar, content, userId }) => (
             <Devit
-              img={img}
-              createdAt={createdAt}
-              content={content}
               avatar={avatar}
-              userName={userName}
-              key={id}
+              createdAt={createdAt}
               id={id}
+              img={img}
+              key={id}
+              content={content}
+              userName={userName}
               userId={userId}
             />
           )
@@ -59,54 +62,51 @@ export default function HomePage() {
           </a>
         </Link>
       </nav>
-      <style jsx>
-        {`
-          header {
-            align-items: center;
-            display: flex;
-            border-bottom: 1px solid #eee;
-            height: 49px;
-            position: sticky;
-            top: 0;
-            width: 100%;
-            background: #ffffffaa;
-            backdrop-filter: blur(5px);
-          }
-          section {
-            flex: 1;
-          }
-          h2 {
-            font-size: 21px;
-            font-weight: 800;
-            padding-left: 15px;
-          }
-
-          nav {
-            background: #fff;
-            bottom: 0;
-            border-top: 1px solid #eee;
-            height: 49px;
-            position: sticky;
-            width: 100%;
-            display: flex;
-          }
-          nav a {
-            align-items: center;
-            height: 100%;
-            display: flex;
-            flex: 1 1 auto;
-            justify-content: center;
-          }
-          nav a:hover {
-            background: radial-gradient(#0099ff22 15%, transparent 16%);
-            background-size: 180px 180px;
-            background-position: center;
-          }
-          nav a:hover > :global(svg) {
-            stroke: ${colors.primary};
-          }
-        `}
-      </style>
+      <style jsx>{`
+        header {
+          align-items: center;
+          background: #ffffffaa;
+          backdrop-filter: blur(5px);
+          border-bottom: 1px solid #eee;
+          height: 49px;
+          display: flex;
+          position: sticky;
+          top: 0;
+          width: 100%;
+        }
+        section {
+          flex: 1;
+        }
+        h2 {
+          font-size: 21px;
+          font-weight: 800;
+          padding-left: 15px;
+        }
+        nav {
+          background: #fff;
+          bottom: 0;
+          border-top: 1px solid #eee;
+          display: flex;
+          height: 49px;
+          position: sticky;
+          width: 100%;
+        }
+        nav a {
+          align-items: center;
+          display: flex;
+          flex: 1 1 auto;
+          height: 100%;
+          justify-content: center;
+        }
+        nav a:hover {
+          background: radial-gradient(#0099ff22 15%, transparent 16%);
+          background-size: 180px 180px;
+          background-position: center;
+        }
+        nav a:hover > :global(svg) {
+          stroke: ${colors.primary};
+        }
+      `}</style>
     </>
   );
 }
